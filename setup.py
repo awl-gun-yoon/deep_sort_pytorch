4356393a9090afa837c1eaec96a8e5cb6d60860c
Gun Yoon <gun.yoon@awl.co.jp>
2020-02-26 14:07:29 +0900

    Make the project as a python package

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='deep_sort_pytorch',
+      version='1.0',
+      description='Deep sort algorithm written in pytorch',
+      author='ZQPei',
+      author_email='dfzspzq@163.com',
+      url='https://github.com/ZQPei/deep_sort_pytorch',
+      packages=['deep_sort_pytorch'],
+      )
